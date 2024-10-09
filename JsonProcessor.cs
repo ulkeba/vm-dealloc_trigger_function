e5c41cf5d2412d12bf982c0c5775641f3f0ea3c0
Bastian Ulke <bastian.ulke@microsoft.com>
2024-10-09 11:17:45 +0200

    Filter events.

--- JsonProcessor.cs
+++ JsonProcessor.cs
@@ -2,6 +2,9 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
 using Azure.Messaging.EventGrid;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
 
 namespace EventProcessorFunction
 {
@@ -11,6 +14,16 @@ namespace EventProcessorFunction
         public void RunEventGridTrigger([EventGridTrigger] EventGridEvent eventGridEvent, ILogger log)
         {
             log.LogInformation($"C# Event grid trigger function Processed event :{eventGridEvent.Data.ToString()}");
+            
+            var jsonParsed = JsonNode.Parse(eventGridEvent.Data);
+
+            var operationName = jsonParsed["operationName"].GetValue<string>();
+            var resourceUri = jsonParsed["resourceUri"].GetValue<string>();
+            log.LogInformation($"Got event for operation {operationName}...");
+            if ("Microsoft.Compute/virtualMachines/deallocate/action".Equals(operationName))
+            {
+                log.LogInformation($"Virtual Machine deallocated: {resourceUri}. Do something here...");
+            }
         }
 
     }
